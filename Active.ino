c4e74629703dcc9ad94284022083ed370b2db336
zwj-state <51753360+zwj-state@users.noreply.github.com>
2019-06-12 23:07:44 +0800

    Add files via upload

---
+++ Active.ino
@@ -0,0 +1,126 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 7
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float tempC = 0;
+
+
+
+#define NOTE_D0 -1
+#define NOTE_D1 262
+#define NOTE_D2 293
+#define NOTE_D3 329
+#define NOTE_D4 349
+#define NOTE_D5 392
+#define NOTE_D6 440
+#define NOTE_D7 494
+#define NOTE_DL1 147
+#define NOTE_DL2 165
+#define NOTE_DL3 175
+#define NOTE_DL4 196
+#define NOTE_DL5 221
+#define NOTE_DL6 248
+#define NOTE_DL7 278
+#define NOTE_DH1 523
+#define NOTE_DH2 586
+#define NOTE_DH3 658
+#define NOTE_DH4 697
+#define NOTE_DH5 783
+#define NOTE_DH6 879
+#define NOTE_DH7 987
+//以上部分是定义是把每个音符和频率值对应起来，这里用NOTE_D+数字表示音符，NOTE_DH+数字表示上面有点的那种音符，NOTE_DL+数字表示下面有点的那种音符。
+
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.25
+#define SIXTEENTH 0.625
+//这部分是用英文对应拍子
+
+
+int tune[] = 
+{
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,
+  NOTE_D1,NOTE_D1,NOTE_D3,
+  NOTE_D1,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D7,NOTE_D7,NOTE_D5,NOTE_D3,
+  NOTE_D5,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D3,NOTE_D3,NOTE_D1,NOTE_DL6,
+  NOTE_D1,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_DH1,NOTE_D0,NOTE_D7,NOTE_D5,
+  NOTE_D6,
+};//这部分就是整首曲子的音符部分，用了一个序列定义为tune，整数
+
+float duration[]=
+{
+  1,1,0.5,0.5,1,
+  0.5,0.5,0.5,0.5,1,0.5,0.5,
+  0.5,1,0.5,1,0.5,0.5,
+  0.5,0.5,0.5,0.5,1,1,
+  1,1,1+1,
+  0.5,1,1+0.5,1,
+  1,1,0.5,0.5,1,
+  0.5,1,1+0.5,1,
+  0.5,0.5,0.5,0.5,1+1,
+  0.5,1,1+0.5,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,1,0.5,1+1,
+  0.5,1,0.5,1+1,
+  1+1,0.5,0.5,1,
+  1+1+1+1
+};//这部分是整首曲子的节拍部分，也定义个序列duration，浮点（数组的个数和前面音符的个数是一样的，一一对应么）
+
+int length;//这里定义一个变量，后面用来表示共有多少个音符
+int tonePin=9;//蜂鸣器的pin
+
+void setup()
+{
+  pinMode(tonePin,OUTPUT);//设置蜂鸣器的pin为输出模式
+  length = sizeof(tune)/sizeof(tune[0]);//这里用了一个sizeof函数，可以查出tone序列里有多少个音符
+
+  sensors.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  sensors.requestTemperatures();
+  tempC = sensors.getTempCByIndex(0);
+  delay(1000);
+  Serial.println(tempC);
+
+
+  if(tempC>30)
+  {
+    for(int x=0;x<length;x++)
+    {
+      tone(tonePin,tune[x]);//此函数依次播放tune序列里的数组，即每个音符
+      delay(400*duration[x]);//每个音符持续的时间，即节拍duration，400是调整时间的越大，曲子速度越慢，越小曲子速度越快，自己掌握吧
+      noTone(tonePin);//停止当前音符，进入下一音符
+    }
+    delay(5000);//等待5秒后，循环重新开始
+   }
+}
